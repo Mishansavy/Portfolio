3ad47e37c56d9732c7a5299917e20198d53329cb
Mishan Shah <smishan7@gmail.com>
2024-04-23 20:33:53 +0545

    img in header

--- Frontend/src/Components/Header/Header.jsx
+++ Frontend/src/Components/Header/Header.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-
+//styles import
+import { HeadMainContainer, ImgDiv } from "../../Styles/Styles";
+// img import
+import mishan from "../../assets/img/mishan.jpg";
 export const Header = () => {
-  return <Head></Head>;
+  return (
+    <HeadMainContainer>
+      <ImgDiv src={mishan} alt="mishan raj shah" />
+    </HeadMainContainer>
+  );
 };
---
+++ Frontend/src/Styles/Styles.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+export const HeadMainContainer = styled.div`
+  width: 100%;
+  height: 200px;
+`;
+export const ImgDiv = styled.img`
+  width: 100px;
+  height: 95px;
+  object-fit: cover;
+  border-radius: 50%;
+`;
+export const Styles = () => {
+  return <div>Styles</div>;
+};
